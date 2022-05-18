0b20ca58738c8851e2bb4fc9bd1dc88b88cf258e
durandaltheta <blaynethomasdennis@gmail.com>
2022-05-18 15:03:38 +0000

    Deploying to gh-pages from @ durandaltheta/sthread@271ea817e0dd42ea478484098a00b2d7bd0ab6c3 🚀

--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['queued_45',['queued',['../structst_1_1channel.html#ae8aadf196a461711905255b1d129026e',1,'st::channel::queued()'],['../structst_1_1worker.html#a8cfc10372a6d1fb26b867313d190acc8',1,'st::worker::queued()']]]
+  ['queued_45',['queued',['../structst_1_1channel.html#ae8aadf196a461711905255b1d129026e',1,'st::channel']]]
 ];
