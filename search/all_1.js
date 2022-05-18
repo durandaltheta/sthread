a2124ef21855891f9289ffbe43fce692d6df407c
durandaltheta <blaynethomasdennis@gmail.com>
2022-05-18 18:25:48 +0000

    Deploying to gh-pages from @ durandaltheta/sthread@b3821f2ab7fa0c0ed15bd927532da1cc21706625 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['channel_2',['channel',['../structst_1_1channel.html',1,'st']]],
   ['close_3',['close',['../structst_1_1channel.html#ae2a319160262483ed2e290d81390c456',1,'st::channel']]],
-  ['closed_4',['closed',['../structst_1_1channel.html#a5055f2993a20b50d85df93d840d2bed8',1,'st::channel']]],
+  ['closed_4',['closed',['../structst_1_1channel.html#af35d1eabf3947d9f5939b14e33966389',1,'st::channel']]],
   ['code_5',['code',['../structst_1_1message.html#ab1038fb1e9e186726582761daed09285',1,'st::message']]],
   ['copy_5fdata_5fto_6',['copy_data_to',['../structst_1_1message.html#a712cfd7aa3954f29645561f4e29a4af3',1,'st::message']]]
 ];
